8a9b29d46a00d679cf0c281d61b0394215d29271
Alex Pitenko <oleksii.p@itsyndicate.org>
2023-03-22 00:57:11 +0100

    implemented board and cells displaying

---
+++ src/boardUtils.ts
@@ -0,0 +1,13 @@
+import { Vector2 } from "three";
+
+// TODO
+class Cell {
+    constructor(public x: number, public y: number) {
+
+    }
+}
+
+// TODO
+export const getCellCenter = (cell: Cell): Vector2 => {
+    return new Vector2(0, 0);
+}
